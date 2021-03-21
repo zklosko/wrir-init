b579e4119bc87e4de9a4ddf3692bae2a8c01aaf3
Zachary Klosko <kloskoz@vcu.edu>
2021-03-20 22:02:30 -0400

    Spring cleaning

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 
 1. Install Ubuntu Server 20.04 LTS
 2. Download repo with `git clone https://github.com/zklosko/wrir-init.git`
-3. Run init file (`bandito-init.sh` or `rostov-init.sh`)
+3. Run init file -> `sudo bash init.sh`
+4. Profit
--- init.sh
+++ init.sh
@@ -1,43 +1,39 @@
 # /bin/bash
 
 # 1. Install prereqs
-apt update -y && apt install ffmpeg tzdata procps id3v2 apache2 icecast2 cifs-utils winbind libnss-winbind -y
+apt update
+apt install -y ffmpeg tzdata procps id3v2 apache2 icecast2 cifs-utils winbind libnss-winbind
 
 # 2. Copy files
 
-# For Icecast
+# Config files
 cp -av icecast.xml /etc/icecast2/
+cp -av files.wrir.org.conf /etc/apache2/sites-enabled/
 
-# For the stream recorder
-cp -avr recorder /
-
-# For Nginx
-# cp -avr html/* /srv/static
-# cp -avr cgi-bin/* /srv/cgi-bin/
-# cp -av default /etc/nginx/sites-configured/
+# Application files
+cp -avr srv/* /srv
 
 # 3. Initialize setup
 
 # Attach network drives
 mkdir /Y && mkdir /Z
 
-touch /.smbcredentials && \
-  echo -e 'username=rfrdj\npassword=wwr4trou\ndomain=wrir.local' > /.smbcredentials
-
+echo -e 'username=rfrdj\npassword=wwr4trou\ndomain=wrir.local' >> /.smbcredentials
 echo "//192.168.200.16/shared /Y cifs credentials=/.smbcredentials,vers=1.0,iocharset=utf8,gid=1000,uid=1000,file_mode=0777,dir_mode=0777 0 0 " >> /etc/fstab
 echo "//192.168.200.23/z /Z cifs credentials=/.smbcredentials,vers=1.0,iocharset=utf8,gid=1000,uid=1000,file_mode=0777,dir_mode=0777 0 0 " >> /etc/fstab
 
 mount -a
 
-# Add RecRunner.sh to crontab
-echo "@reboot root bash /recorder/RecRunner.sh &" >> /etc/crontab
+# Set up cron
+cat crontab > /etc/crontab
 
 # Set up firewall
 ufw enable
 ufw allow 22,80,8000/tcp
 
 # Restart services
-systemctl restart nginx
+systemctl restart apache2
 systemctl restart icecast2
+systemctl restart cron
 
 echo 'All done!'
