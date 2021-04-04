2517018fb9796321c37669905a919f521bd2d3bf
Zachary Klosko <kloskoz@vcu.edu>
2021-04-04 14:53:41 -0400

    Adding raw directory cleaning

--- srv/recorder/Cleaner.sh
+++ srv/recorder/Cleaner.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
 # Cleaning files in raw
-sDays=75
+sDays=1
 echo Starting $0 $(date)
 
-sOldFiles=$(find ./raw/* -ctime +${sDays})  # refractered line
-[[ -n "${sOldFiles}" ]] && rm ${sOldFiles}
+find /srv/recorder/raw/* -ctime ${sDays} -delete
 
 # Cleaning files in publish a.k.a. files.wrir.org/shows
-sDir="/srv/static/shows"
+# sDir="/srv/static/shows"
 
-echo "Starting $0 $(date)"
+# echo "Starting $0 $(date)"
 
-cd "${sDir}" || exit
+# cd "${sDir}" || exit
 
-sLimit="95"
-while sPCFree=$(df . --output=pcent|tail -1) && [[ ${sPCFree%\%} -ge ${sLimit} ]]
-do
-  echo "Free space: ${sPCFree}"
-  sDel=$(ls -tr ./ | head -1)
-  echo "delete ${sDel}"
-  rm "${sDel}"
-done
+# sLimit="95"
+# while sPCFree=$(df . --output=pcent|tail -1) && [[ ${sPCFree%\%} -ge ${sLimit} ]]
+# do
+#   echo "Free space: ${sPCFree}"
+#   sDel=$(ls -tr ./ | head -1)
+#   echo "delete ${sDel}"
+#   rm "${sDel}"
+# done
 
-echo Ending $0 $(date)
+# echo Ending $0 $(date)
